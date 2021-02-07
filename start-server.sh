e2e7e6787f878a1b900198ab0004773a9d0521e6
nopor <github@nopor.de>
2021-02-07 18:26:36 +0100

    Updated start script

--- start-server.sh
+++ start-server.sh
@@ -6,11 +6,11 @@ cp /home/valheim/data/linux64/steamclient.so /home/valheim/data
 export LD_LIBRARY_PATH=$temp_ldpath
 
 echo "### Starting Valheim server ###"
-while true; do 
-  ./data/valheim_server.x86_64 \
+./data/valheim_server.x86_64 \
   -name \"${SERVER_NAME}\" \
   -port ${SERVER_PORT} \
   -world \"${SERVER_WORLD}\" \
   -password \"${SERVER_PASSWORD}\" \
-  -public 1
-done
+  -public 1 > /dev/null &
+
+while wait $!; [ $? != 0 ]; do true; done
