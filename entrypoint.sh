901cc59438f0c152c3780c35ecb09d844a899983
jazo10 <jazo10@users.noreply.github.com>
2021-02-08 13:32:28 +1100

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Downloading/updating server on startup
-
-echo "### Downloading Valheim Server ###"
-steamcmd +login anonymous +force_install_dir /home/valheim/data +app_update 896660 validate +quit
-
-exec "$@"
